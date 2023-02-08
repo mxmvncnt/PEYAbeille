7acb359c53a756b7c313dbaa7ae36425db2fbfc6
Maxime Vincent <git@maximevincent.com>
2023-02-08 16:24:56 -0500

    creer server.js a partir du labo2

---
+++ Server/server.js
@@ -0,0 +1,39 @@
+/**
+* importation des modules requis
+**/
+const express = require('express');
+const app = express()
+const http = require('http');
+const oracledb = require('oracledb');
+const bodyParser = require('body-parser')
+const dateFormat = require('dateformat');
+const dateNow = new Date();
+
+// const con = oracledb.createConnection({
+//     host: "127.0.0.1",
+//     user: "scott",
+//     password: "oracle",
+//     database: "mybd",
+// });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs')
+
+
+const siteTitle = "Application simple";
+const baseURL = "http://localhost:4000/";
+
+app.get('/', function (req, res) {
+    result = "Requete SQL"
+    res.render('../Pages/Accueil/index.ejs', {
+        siteTitle: siteTitle,
+        pageTitle: "Event list",
+        items: result
+    });
+});
+
+
+const server = app.listen(4000, function () {
+    console.log("serveur fonctionne sur 4000... ! ");
+});
