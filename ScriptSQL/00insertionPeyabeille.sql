80390a6ecf5d0d21aa242d615506116d4410df3e
Maxime Vincent <git@maximevincent.com>
2023-02-15 16:13:25 -0500

    corriger le script d'insertion

--- ScriptSQL/00insertionPeyabeille.sql
+++ ScriptSQL/00insertionPeyabeille.sql
@@ -1,18 +1,85 @@
+INSERT INTO produit (
+    id_produit, 
+    nom, 
+    description, 
+    prix_suggere, 
+    prix_fixe, 
+    url_catalog, 
+    inventaire, 
+    quantite
+)
+values( 
+    12, 
+    'miel de manuka',
+    'Miel excellent pour la peau gout corse',
+    15,
+    20,
+    '.....',
+    1000,
+    1000
+);
 
+INSERT INTO produit (
+    id_produit,
+    nom, 
+    description, 
+    prix_suggere, 
+    prix_fixe, 
+    url_catalog, 
+    inventaire, 
+    quantite
+)
+values( 
+    13, 
+    "miel d'eucalyptus", 
+    'description ici', 
+    15, 
+    20, 
+    '.....', 
+    100, 
+    100
+);
 
-----
-INSERT INTO produit (id_produit,nom,description,prix_suggere,prix_fixe,url_catalog, inventaire,
-quantite) values( 12, 'miel de manuka','Miel excellent pour la peau gout corse','15','20','.....','1000','1000');
+INSERT INTO produit (
+    id_produit, 
+    nom, 
+    description, 
+    prix_suggere, 
+    prix_fixe, 
+    url_catalog, 
+    inventaire,
+    quantite
+)
+values (
+    14, 
+    'miel de tournesol',
+    'description a inserer ici', 
+    16,
+    20,
+    '....',
+    100,
+    100
+);
 
-INSERT INTO produit (id_produit,nom,description,prix_suggere,prix_fixe,url_catalog, inventaire,
-quantite) values( 13, 'miel d'eucalyptus', 'description ici','15','20','.....','100','100');
-
-INSERT INTO produit (id_produit, nom, description, prix_suggere, prix_fix, url_catalog, inventaire, quantite)
-values (14, 'miel de tournesol' ,'description a inserer ici', '16','20' ,'....','100','100');
-
-INSERT INTO produit(id_produit, nom,description, prix_suggere, prix_fixe, url_catalog, inventaire, quantite)
-values (15, 'miel de fleurs sauvages','description a inserer ici','23','30','...','200','200');
+INSERT INTO produit (
+    id_produit, 
+    nom, 
+    description, 
+    prix_suggere, 
+    prix_fixe, 
+    url_catalog, 
+    inventaire, 
+    quantite
+)
+values (
+    15, 
+    'miel de fleurs sauvages',
+    'description a inserer ici',
+    23,
+    30,
+    '...',
+    200,
+    200
+);
 
 commit;
-
-
