ca16fdd975bb7983c5db8a5e4139488996d43c3e
Maxime Vincent <git@maximevincent.com>
2023-02-15 17:53:35 -0500

    ajouter connexion BD Oracle pour la page produits

--- Server/server.js
+++ Server/server.js
@@ -9,59 +9,80 @@ const bodyParser = require('body-parser')
 const dateFormat = require('dateformat');
 const dateNow = new Date();
 
-// const con = oracledb.createConnection({
-//     host: "127.0.0.1",
-//     user: "scott",
-//     password: "oracle",
-//     database: "mybd",
-// });
+let libPath;
+if (process.platform === "win32") {
+  // Windows
+  libPath = "C:\\oracle\\instantclient_19_17";
+} else if (process.platform === "darwin") {
+  // macOS
+  libPath = process.env.HOME + "/instantclient_19_8";
+}
+if (libPath && fs.existsSync(libPath)) {
+  oracledb.initOracleClient({ libDir: libPath });
+}
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("Global")); // css global
+async function run (){
 
-app.set('view engine', 'ejs')
-
-
-const siteTitle = "Application simple";
-const baseURL = "http://localhost:4000/";
-
-app.get('/', function (req, res) {
-    result = "Requete SQL"
-    res.render('../Pages/Client/Accueil/accueil.ejs', {
-        siteTitle: siteTitle,
-        pageTitle: "Event list",
-        items: result
+    const con = await oracledb.getConnection({
+        host: "localhost",
+        user: "scott",
+        password: "oracle",
+        connectString: "localhost/orcl"
     });
-});
-
-app.get('/produits', function (req, res) {
-    result = "Requete SQL"
-    res.render('../Pages/Client/Produits/produits.ejs', {
-        siteTitle: siteTitle,
-        pageTitle: "Event list",
-        items: result
+    
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static("Global")); // css global
+    
+    app.set('view engine', 'ejs')
+    
+    
+    const siteTitle = "Application simple";
+    const baseURL = "http://localhost:4000/";
+    
+    app.get('/', function (req, res) {
+        result = "Requete SQL"
+        res.render('../Pages/Client/Accueil/accueil.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Event list",
+            items: result
+        });
     });
-});
-
-app.get('/apropos', function (req, res) {
-    result = "Requete SQL"
-    res.render('../Pages/Client/APropos/aPropos.ejs', {
-        siteTitle: siteTitle,
-        pageTitle: "Event list",
-        items: result
+    
+    app.get('/produits', async function (req, res) {
+        let result = await con.execute("SELECT * FROM produit");
+        console.log(result)
+        res.render('../Pages/Client/Produits/produits.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Produits",
+            items: result
+        });
     });
-});
-
-app.get('/nousjoindre', function (req, res) {
-    result = "Requete SQL"
-    res.render('../Pages/Client/NousJoindre/nousJoindre.ejs', {
-        siteTitle: siteTitle,
-        pageTitle: "Event list",
-        items: result
+    
+    
+    app.get('/apropos', function (req, res) {
+        result = "Requete SQL"
+        res.render('../Pages/Client/APropos/aPropos.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Event list",
+            items: result
+        });
     });
-});
+    
+    app.get('/nousjoindre', function (req, res) {
+        result = "Requete SQL"
+        res.render('../Pages/Client/NousJoindre/nousJoindre.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Event list",
+            items: result
+        });
+    });
+    
+    const server = app.listen(4000, function () {
+        console.log("Serveur en marche...");
+        console.log(baseURL);
+    });
+}
 
-const server = app.listen(4000, function () {
-    console.log("Serveur en marche...");
-    console.log(baseURL);
-});
+
+
+run();
