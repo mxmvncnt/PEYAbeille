d46dcf1db10eb48e2b8699d1b305fb23f8890ff6
Maxime Vincent <git@maximevincent.com>
2023-02-23 16:42:08 -0500

    ajouter module fs serveur

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
 const oracledb = require('oracledb');
+const fs = require('fs')
 
 let libPath;
 if (process.platform === "win32") {
