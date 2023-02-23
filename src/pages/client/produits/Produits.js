75f68d87a94a80c2c43bc16ede250e38fbbe8b6b
Maxime V <git@maximevincent.com>
2023-02-23 16:17:49 -0500

    page produit

--- src/pages/client/produits/Produits.js
+++ src/pages/client/produits/Produits.js
@@ -1,10 +1,23 @@
 import React from "react";
 import './Style.css'
+import OracleDB from "oracledb";
 
 export default function Produits() {
     return (
-        <div class="">
-
+        <div>
+            <h1>Produits</h1>
+            <div class="grid-produits">
+                <div class="produit">
+                    <h2 class="titre">
+                    </h2>
+                    <h3 class="prix">
+                    </h3>
+                    <a href="/produit/<%= item.ID_PRODUIT %>">
+                        <button class="btn-acheter">Acheter</button>
+                    </a>
+                </div>
+            </div>
         </div>
+
     );
 }
