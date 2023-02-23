25fe12add9fa358e274bbfac70747b6e9b5d3bfc
Maxime Vincent <git@maximevincent.com>
2023-02-23 16:54:28 -0500

    corriger dernier warning

--- src/components/header_bar/HeaderBar.js
+++ src/components/header_bar/HeaderBar.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 
 export default function HeaderBar() {
     return (
-        <div class="navigation-bar">
+        <div className="navigation-bar">
             <Link
                 to="/">
                 PEYABEILLE
