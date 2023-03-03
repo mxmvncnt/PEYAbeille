336e52c6613057ac6c3ecba76d1b85d0bba0185f
Maxime Vincent <git@maximevincent.com>
2023-03-03 00:29:06 -0500

    Rendre headerbar adaptatif aux mobiles

--- src/components/header_bar/HeaderBar.js
+++ src/components/header_bar/HeaderBar.js
@@ -1,32 +1,63 @@
 // import { useState } from "react"
 import "./HeaderBar.css"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 
 
 export default function HeaderBar() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return (
         <div className="navigation-bar">
+
+            <button
+                className="hamburger"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded)
+                }}>☰</button>
+
+            {/* LOGO A LA PLACE DU TEXTE "PEYABEILLE"*/}
             <Link
-                to="/">
+                to="/"
+                id="navigation-bar-logo"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded)
+                }}>
                 PEYABEILLE
             </Link>
-            <Link
-                to="/">
-                Accueil
-            </Link>
-            <Link
-                to="/produits">
-                Produits
-            </Link>
-            <Link
-                to="/apropos">
-                À Propos
-            </Link>
-            <Link
-                to="/nousjoindre">
-                Nous joindre
-            </Link>
+
+            {/* inspiré de: https://blog.logrocket.com/create-responsive-navbar-react-css/ */}
+            <div className={isNavExpanded ? "navigation-bar-menu expanded" : "navigation-bar-menu"}>
+                <Link
+                    to="/"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded)
+                    }}>
+                    Accueil
+                </Link>
+                <Link
+                    to="/produits"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded)
+                    }}>
+                    Produits
+                </Link>
+                <Link
+                    to="/apropos"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded)
+                    }}>
+                    À Propos
+                </Link>
+                <Link
+                    to="/nousjoindre"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded)
+                    }}>
+                    Nous joindre
+                </Link>
+            </div>
         </div>
     );
 }
