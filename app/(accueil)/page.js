6afe782207ec8717ca9550275c6e200904852b0a
Maxime Vincent <git@maximevincent.com>
2023-03-04 16:30:54 -0500

    deplacer page accueil

--- app/(accueil)/page.js
+++ app/(accueil)/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../styles/accueil.module.css';
+import styles from '../../styles/accueil.module.css';
 
 export default function Accueil() {
     return (
