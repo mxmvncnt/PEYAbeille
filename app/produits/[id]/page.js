fc0e2cac84fd7b093a454b8c3ce4697baf33d57e
Maxime Vincent <git@maximevincent.com>
2023-03-04 16:05:56 -0500

    style page produit

--- app/produits/[id]/page.js
+++ app/produits/[id]/page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '../../../styles/produit.module.css'
 import Link from "next/link";
+import Image from "next/image";
 import { getInfosProduit } from "../../../server/Api";
 import IndicateurPrix from "../../../components/IndicateurPrix";
 import styles from '../../../styles/produit.module.css'
+import mtl_bg from '../../../public/Montreal-estival.jpg'
 
 export default async function Produit({params}) {
     // let [searchParams] = useSearchParams();
@@ -12,12 +14,12 @@ export default async function Produit({params}) {
     const data = await getInfosProduit(params.id);
 
     return (
-        <div className={styles.page_produit_grid_item_images_slider}>
+        <div className={styles.page_produit}>
 
-            <div className="page_produit_grid">
+            <div className={styles.page_produit_grid}>
 
                 {/* Section Images */}
-                <div className="page_produit_grid_item page_produit_grid_item_images">
+                <div className={`${styles.page_produit_grid_item} ${styles.page_produit_grid_item_images}`}>
                     {/* Code pris et modifié de: https://css_tricks.com/can_get_pretty_far_making_slider_just_html_css/ */}
                     {/* Remplacer cela par une boucle quand nous avons de vraies images */}
 
@@ -28,27 +30,52 @@ export default async function Produit({params}) {
                     <a href="#image_4">4</a>
                     <a href="#image_5">5</a> */}
 
-                    <div className="page_produit_grid_item_images_slider">
-                        <div className="page_produit_grid_item_images_slider_item" id="slide_1"><img src="https://www.airtransat.com/getmedia/cafc7e6e_d0ff_497e_9998_e708f41aa191/Montreal_estival.aspx" alt=""/></div>
-                        <div className="page_produit_grid_item_images_slider_item" id="slide_2"><img src="https://www.airtransat.com/getmedia/cafc7e6e_d0ff_497e_9998_e708f41aa191/Montreal_estival.aspx" alt=""/></div>
-                        <div className="page_produit_grid_item_images_slider_item" id="slide_3"><img src="https://www.airtransat.com/getmedia/cafc7e6e_d0ff_497e_9998_e708f41aa191/Montreal_estival.aspx" alt=""/></div>
-                        <div className="page_produit_grid_item_images_slider_item" id="slide_4"><img src="https://www.airtransat.com/getmedia/cafc7e6e_d0ff_497e_9998_e708f41aa191/Montreal_estival.aspx" alt=""/></div>
-                        <div className="page_produit_grid_item_images_slider_item" id="slide_5"><img src="https://www.airtransat.com/getmedia/cafc7e6e_d0ff_497e_9998_e708f41aa191/Montreal_estival.aspx" alt=""/></div>
+                    <div className={styles.page_produit_grid_item_images_slider}>
+                        <div className={styles.page_produit_grid_item_images_slider_item} id="slide_1">
+                            <Image 
+                                src={mtl_bg}
+                                alt={"test"}
+                            />
+                        </div>
+                        <div className={styles.page_produit_grid_item_images_slider_item} id="slide_1">
+                            <Image 
+                                src={mtl_bg}
+                                alt={"test"}
+                            />
+                        </div>
+                        <div className={styles.page_produit_grid_item_images_slider_item} id="slide_1">
+                            <Image 
+                                src={mtl_bg}
+                                alt={"test"}
+                            />
+                        </div>
+                        <div className={styles.page_produit_grid_item_images_slider_item} id="slide_1">
+                            <Image 
+                                src={mtl_bg}
+                                alt={"test"}
+                            />
+                        </div>
+                        <div className={styles.page_produit_grid_item_images_slider_item} id="slide_1">
+                            <Image 
+                                src={mtl_bg}
+                                alt={"test"}
+                            />
+                        </div>
                     </div>
                 </div>
 
                 {/* Section Infos Produit */}
-                <div className="page_produit_grid_item page_produit_grid_item_infos">
+                <div className={`${styles.page_produit_grid_item} ${styles.page_produit_grid_item_infos}`}>
                     <h1>{data["NOM"]}</h1>
                     <p>{data["DESCRIPTION"]}</p>
                 </div>
 
                 {/* Section ACHETER */}
-                <div className="page_produit_grid_item page_produit_grid_item_acheter">
+                <div className={`${styles.page_produit_grid_item} ${styles.page_produit_grid_item_acheter}`}>
                     <div>
                         <IndicateurPrix prix_regulier={data["PRIX_FIXE"]} prix_suggere={data["PRIX_SUGGERE"]} />
                         <a>
-                            <button className="btn_acheter">ACHETER</button>
+                            <button className="btn-acheter">ACHETER</button>
                         </a>
                         <small>En stock: {data["QUANTITE"]}</small>
                     </div>
