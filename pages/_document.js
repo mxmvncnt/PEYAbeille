1e04987deb37546ed71ba8c329b083f130fa7bf6
Maxime Vincent <git@maximevincent.com>
2023-03-04 12:57:32 -0500

    ajouter layout global

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function document() {
+export default class MyDocument extends Document {
+  render() {
     return (
-        <Html>
-            <Head>
-                <meta charset="utf-8" />
-                <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="theme-color" content="#000000" />
-                <meta
-                    name="description"
-                    content="Web site created using create-react-app"
-                />
-                <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-                <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-                <title>Peyabeille</title>
-            </Head>
-
-            <Main>
-                <NextScript />
-            </Main>
-        </Html>
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     )
+  }
 }
