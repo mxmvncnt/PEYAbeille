12e69a52389ed1ab992fa45a4b7e2c9617a3d06f
Maxime Vincent <git@maximevincent.com>
2023-03-04 16:59:53 -0500

    Ajouter style page produits

--- app/produits/page.js
+++ app/produits/page.js
@@ -3,20 +3,21 @@ import '../../styles/produits.module.css'
 import Link from "next/link";
 import { getProduits } from "../../server/Api";
 import IndicateurPrix from "../../components/IndicateurPrix";
+import styles from '../../styles/produits.module.css'
 
 export default async function Produits() {
     const data = await getProduits();
 
     return (
-        <div className="page-produits-page-produits">
+        <div className={styles.page_produits_page_produits}>
             <h1>Produits</h1>
 
-            <div className="page-produits-grid-produits">
+            <div className={styles.page_produits_grid_produits}>
                 {data.map((item) => (
                     <div key={item["ID_PRODUIT"]}>
 
-                        <div className="page-produits-produit">
-                            <h2 className="page-produits-titre">
+                        <div className={styles.page_produits_produit}>
+                            <h2 className={styles.page_produits_titre}>
                                 {item["NOM"]}
                             </h2>
                             <IndicateurPrix prix_regulier={item["PRIX_FIXE"]} prix_suggere={item["PRIX_SUGGERE"]} />
