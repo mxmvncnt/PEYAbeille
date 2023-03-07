883f6c3ded5f3574c2b61e2390a5753aa7b31739
Maxime Vincent <git@maximevincent.com>
2023-03-07 17:37:18 -0500

    creation composant titre accueil

---
+++ components/titreAccueil.js
@@ -0,0 +1,19 @@
+// import { useState } from "react"
+import styles from '../styles/accueil.module.css'
+
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+
+export default function TitreAccueil() {
+    return (
+        <div className="">
+            <ParallaxProvider>
+                <Parallax scale={[1, 0.5, 'easeInOutQuad']} translateX={[50, 0]}>
+                    <p className={styles.nom_site}>Pey</p>
+                </Parallax>
+                <Parallax scale={[1, 0.2, 'easeInOutQuad']}>
+                    <p className={styles.nom_site}>abeille</p>
+                </Parallax>
+            </ParallaxProvider>
+        </div>
+    );
+}
