3781fa93ceddde7c7952eb71fdeb2e9b9c57e068
Maxime Vincent <git@maximevincent.com>
2023-03-08 16:42:08 -0500

    creer pages vides connexion et inscription

---
+++ app/auth/connexion/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../../styles/connexion.module.css';
+
+export default function Connexion() {
+    return (
+        <div className={styles.nom_classe}>
+
+        </div>
+    );
+}
---
+++ app/auth/inscription/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../../styles/inscription.module.css';
+
+export default function Inscription() {
+    return (
+        <div className={styles.nom_classe}>
+
+        </div>
+    );
+}
