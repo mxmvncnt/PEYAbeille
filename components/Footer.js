7c083bd172dddd2de7c162466695dfd2bf729f38
Maxime Vincent <git@maximevincent.com>
2023-03-08 16:04:03 -0500

    ajouter style footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,8 +5,8 @@ import styles from '../styles/footer.module.css'
 
 export default function Footer() {
     return (
-        <div className="footer">
-            <div className="footer-categorie footer-liens">
+        <div className={styles.footer}>
+            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
                 <h3>Liens</h3>
                 <a href="example.com">
                     <div>Instagram</div>
@@ -15,13 +15,13 @@ export default function Footer() {
                     <div>Facebook</div>
                 </a>
             </div>
-            <div className="footer-categorie footer-politiques">
+            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
                 <h3>Politiques</h3>
                 <a href="example.com">
                     <div>Conditions d'utilisation</div>
                 </a>
             </div>
-            <div className="footer-categorie footer-contact">
+            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
                 <h3>Contact</h3>
                 <form action="">
                     <label>Courriel</label>
