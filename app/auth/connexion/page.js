2bf658a3d67d3d39e8c9ecf74fb03d9a07e68453
Maxime Vincent <git@maximevincent.com>
2023-03-10 13:46:57 -0500

    Reparer lien fichier css

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../../styles/connexion.module.css';
+import styles from '../../../styles/connexion.module.css';
 
 export default function Connexion() {
     return (
