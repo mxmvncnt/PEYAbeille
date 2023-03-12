5fa9bab8074a9deb1d35f887fb62a43a5a58a764
Maxime Vincent <git@maximevincent.com>
2023-03-12 16:01:39 -0400

    Utiliser requete GET pour l'envoi du formulaire

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -5,10 +5,10 @@ export default function Connexion() {
     return (
         <div className={styles.nom_classe}>
             <h1>Connexion</h1>
-            <form action={`/api/login`} method="POST">
-                <label for="courriel">Courriel:</label>
+            <form action={`/api/login`} method="GET">
+                <label htmlFor="courriel">Courriel:</label>
                 <input type="email" id="courriel" name="email"/>
-                <label for="mdp">Mot de passe:</label>
+                <label htmlFor="mdp">Mot de passe:</label>
                 <input type="password" id="mdp" name="password"/>
                 <input type="submit" value="Envoyer" />
             </form>
