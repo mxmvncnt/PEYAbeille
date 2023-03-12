97429f95c197929aa5b904f2589aca0f3ba20652
Maxime Vincent <git@maximevincent.com>
2023-03-12 15:27:06 -0400

    ajouter formulaire basique de login

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -4,7 +4,14 @@ import styles from '../../../styles/connexion.module.css';
 export default function Connexion() {
     return (
         <div className={styles.nom_classe}>
-
+            <h1>Connexion</h1>
+            <form action={`/api/login`} method="POST">
+                <label for="courriel">Courriel:</label>
+                <input type="email" id="courriel" name="email"/>
+                <label for="mdp">Mot de passe:</label>
+                <input type="password" id="mdp" name="password"/>
+                <input type="submit" value="Envoyer" />
+            </form>
         </div>
     );
 }
