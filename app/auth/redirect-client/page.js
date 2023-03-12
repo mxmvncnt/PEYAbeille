64df3d4d5f0166ff15c3f74625f2d18fb6486742
Maxime Vincent <git@maximevincent.com>
2023-03-12 15:49:00 -0400

    renommer le cookie token dans la redirections

--- app/auth/redirect-client/page.js
+++ app/auth/redirect-client/page.js
@@ -8,9 +8,10 @@ import { redirect } from "next/navigation";
  * Page de redirection faite pour mettre un cookie sur le navigateur
  */
 export default function RedirectClient(token) {
+    
 
-    const [cookies, setCookie] = useCookies(['access_token'])
-    setCookie('access_token', token, {sameSite: true})
+    const [cookies, setCookie] = useCookies(['token'])
+    setCookie('token', token, {sameSite: true})
 
     redirect('/')
 }
