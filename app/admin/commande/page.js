bbe7be45879c6d04cfb0888787ddfe0a10416edb
Mihaja514 <67440213+Mihaja514@users.noreply.github.com>
2023-03-14 14:02:34 -0400

    commande

--- app/admin/commande/page.js
+++ app/admin/commande/page.js
@@ -3,6 +3,32 @@ import styles from '../../../styles/commande.module.css';
 
 export default function commandes(){
     return(
+
+        <div>
             <h1> Commandes</h1>
+
+            <div className={styles.commande}>
+                <h2> Nom du miel (#001)</h2>
+                <button className={styles.bttnCompleter}> Completer </button>
+                <button className={styles.bttnAnnuler}> Annuler </button>
+                <p>Paul Nord <br/>1234 rue des mirabels
+                <br/>514 514 5140 </p>
+            </div>
+            export commande();
+            
+        </div>
+
     );
+}
+
+ function commande(){
+    return(
+        <div className={styles.commande}>
+        <h2> Nom du miel (#001)</h2>
+        <button className={styles.bttnCompleter}> Completer </button>
+        <button className={styles.bttnAnnuler}> Annuler </button>
+        <p>Paul Nord <br/>1234 rue des mirabels
+        <br/>514 514 5140 </p>
+    </div>
+    )
 }
