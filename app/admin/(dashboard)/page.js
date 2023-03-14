3f9c227ebc9444ae5d873915a864a36348decfd9
Maxime Vincent <git@maximevincent.com>
2023-03-14 18:48:34 -0400

    ajouter data au dashboard admin

--- app/admin/(dashboard)/page.js
+++ app/admin/(dashboard)/page.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { cookies } from 'next/headers';
+import { getAdminStats } from '../../../server/Api'
 import styles from '../../../styles/dashboard.module.css';
 
+const getToken = () => {
+    const nextCookies = cookies(); // Get cookies object
+    const token = nextCookies.get('token') // Find cookie
+    return token
+}
 
+export default async function dashboard() {
+    const token = getToken();
 
-export default function dashboard(){
+    const data = await getAdminStats(token["value"]);
+
     return(
         <div>
             <h1>Dashboard</h1>
             <div className={styles.dashboard_container}>
-                <p>Volume de ventes total : </p>
-                <p>Nombre de commandes total :</p>
-                <p>Produit le plus populaire : </p>
-                <p>Produit le moins populare : </p>
+                <p>Volume de ventes total: {data["ventes"]}</p>
+                <p>Nombre de commandes total: {data["commandes"]}</p>
+                <p>Produit le plus populaire: {data["produits"]["plus_populaire"]["produit"]["nom"]} ({data["produits"]["plus_populaire"]["ventes"]} ventes)</p>
+                <p>Produit le moins populare: {data["produits"]["moins_populaire"]["produit"]["nom"]} ({data["produits"]["moins_populaire"]["ventes"]} ventes)</p>
             </div>
             <div className={styles.bouttons}>
                 <div className={styles.AllerCommande}>
