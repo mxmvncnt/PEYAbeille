9dd216f38a05be5be34baffa9b6dc3cd73b93b59
Mihaja514 <67440213+Mihaja514@users.noreply.github.com>
2023-03-14 00:52:24 -0400

    page commande

---
+++ app/admin/commande/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from '../../../styles/commande.module.css';
+
+export default function commandes(){
+    return(
+            <h1> Commandes</h1>
+    );
+}
