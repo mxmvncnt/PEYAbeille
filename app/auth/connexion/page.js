8132a6d5e9ca45647ddc4096aadaf1a293890380
MihailPeychev <78099428+MihailPeychev@users.noreply.github.com>
2023-03-15 17:43:31 -0400

    Debut Page connexion

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from '../../../styles/connexion.module.css';
 
+
 export default function Connexion() {
     return (
-        <div className={styles.nom_classe}>
-            <h1>Connexion</h1>
-            <form action={`/api/login`} method="GET">
-                <label htmlFor="courriel">Courriel:</label>
-                <input type="email" id="courriel" name="email"/>
-                <label htmlFor="mdp">Mot de passe:</label>
-                <input type="password" id="mdp" name="password"/>
-                <input type="submit" value="Envoyer" />
-            </form>
+        <div className={styles.body}>
+
+            <div className={styles.container}>
+                <h1>Connexion</h1>
+                <form action={`/api/login`} method="GET">
+                    <input className={styles.input} type="email" id="courriel" name="email" placeholder="Courriel" />
+                    <br></br>
+                    <label htmlFor="mdp">Mot de passe:</label>
+                    <input type="password" id="mdp" name="password" />
+                    <input type="submit" value="Envoyer" />
+                </form>
+            </div>
         </div>
     );
 }
