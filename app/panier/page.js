05792f46c274cb99ae1ec08f6be10b99cf39fe40
Maxime Vincent <git@maximevincent.com>
2023-03-16 15:04:07 -0400

    Creer base page panier

---
+++ app/panier/page.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { cookies } from 'next/headers';
+import styles from '../../styles/panier.module.css';
+
+export default async function Panier() {
+
+    return (
+        <div className={styles.body}>
+            <h1>Panier</h1>
+
+            <div className={styles.sections_panier}>
+
+                <div className={styles.infos_commande}>
+                    <h2 className={styles.titre_section}>Adresse</h2>
+                    <form action={``} method="GET">
+
+                        <label htmlFor="prenom">Prénom:</label>
+                        <input type="text" id="prenom" name="prenom" />
+
+                        <label htmlFor="nom">Nom:</label>
+                        <input type="text" id="nom" name="nom" />
+
+                        <label htmlFor="numeroRue">Numéro de rue:</label>
+                        <input type="text" id="numeroRue" name="numeroRue" />
+
+                        <label htmlFor="rue">Rue:</label>
+                        <input type="text" id="rue" name="rue" />
+
+                        <input type="submit" value="Envoyer" />
+                    </form>
+                </div>
+
+                <div className={styles.items_commande}>
+                    <h2 className={styles.titre_section}>Items</h2>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
