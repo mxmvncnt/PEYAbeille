8fec0e7b53e8622a2579f5108679a945ce555abe
Mihaja514 <67440213+Mihaja514@users.noreply.github.com>
2023-03-16 16:30:37 -0400

    commande

--- app/admin/commande/page.js
+++ app/admin/commande/page.js
@@ -1,34 +1,60 @@
-import React from "react";
+'use client'
+import React,{useState} from "react";
 import styles from '../../../styles/commande.module.css';
 
-export default function commandes(){
-    return(
+
+export default function commandes() {
+    return (
 
         <div>
             <h1> Commandes</h1>
 
-            <div className={styles.commande}>
-                <h2> Nom du miel (#001)</h2>
-                <button className={styles.bttnCompleter}> Completer </button>
-                <button className={styles.bttnAnnuler}> Annuler </button>
-                <p>Paul Nord <br/>1234 rue des mirabels
-                <br/>514 514 5140 </p>
-            </div>
-            export commande();
-            
+            <Commande />
+            <Commande />
+
         </div>
 
     );
 }
 
- function commande(){
-    return(
+
+function Commande() {
+    return (
         <div className={styles.commande}>
-        <h2> Nom du miel (#001)</h2>
-        <button className={styles.bttnCompleter}> Completer </button>
-        <button className={styles.bttnAnnuler}> Annuler </button>
-        <p>Paul Nord <br/>1234 rue des mirabels
-        <br/>514 514 5140 </p>
-    </div>
+            <h2> Nom du client (# numero de commande)</h2>
+            <div className={styles.bouttons}>
+            <button className={styles.bttnCompleter}> Completer </button>
+            <button className={styles.bttnAnnuler}> Annuler </button>
+            </div>
+
+            <p>Paul Nord <br />1234 rue des mirabels
+            <br />514 514 5140 </p>
+            <Details/>
+
+        </div>
     )
 }
+
+const Details = () => {
+    const [toggle, setToggle] = useState(false)
+    return (
+        <div>
+            <button onClick={() => setToggle(!toggle)}>Voir detail</button>
+            {toggle && (
+                <AfficherDetails/>
+            )}
+        </div>
+    )
+}
+
+function AfficherDetails() {
+    return(
+        <div>
+            <h3>test</h3>
+        </div>
+
+    )
+}
+
+
+
