36a94e1293727d17c35bdf113122b9189d07c531
Maxime Vincent <git@maximevincent.com>
2023-03-16 15:03:58 -0400

    retourner message d'erreur si non autorise dashboard admin

--- app/admin/(dashboard)/page.js
+++ app/admin/(dashboard)/page.js
@@ -12,29 +12,37 @@ const getToken = () => {
 export default async function dashboard() {
     const token = getToken();
 
-    const data = await getAdminStats(token["value"]);
-
-    return(
-        <div>
-            <h1>Dashboard</h1>
-            <div className={styles.dashboard_container}>
-                <p>Volume de ventes total: {data["ventes"]}</p>
-                <p>Nombre de commandes total: {data["commandes"]}</p>
-                <p>Produit le plus populaire: {data["produits"]["plus_populaire"]["produit"]["nom"]} ({data["produits"]["plus_populaire"]["ventes"]} ventes)</p>
-                <p>Produit le moins populare: {data["produits"]["moins_populaire"]["produit"]["nom"]} ({data["produits"]["moins_populaire"]["ventes"]} ventes)</p>
-            </div>
-            <div className={styles.bouttons}>
-                <div className={styles.AllerCommande}>
-                    <h3> Dernieres commandes</h3>
-                    <button className={styles.button}> Go!</button>
+    if (token != null) {
+        const data = await getAdminStats(token["value"]);
+        return(
+            <div>
+                <h1>Dashboard</h1>
+                <div className={styles.dashboard_container}>
+                    <p>Volume de ventes total: {data["ventes"]}</p>
+                    <p>Nombre de commandes total: {data["commandes"]}</p>
+                    <p>Produit le plus populaire: {data["produits"]["plus_populaire"]["produit"]["nom"]} ({data["produits"]["plus_populaire"]["ventes"]} ventes)</p>
+                    <p>Produit le moins populare: {data["produits"]["moins_populaire"]["produit"]["nom"]} ({data["produits"]["moins_populaire"]["ventes"]} ventes)</p>
                 </div>
-                <div className={styles.AllerInventaire}>
-                    <h3> Inventaire</h3>
-                    <button className={styles.button}> Go!</button>
+                <div className={styles.bouttons}>
+                    <div className={styles.AllerCommande}>
+                        <h3> Dernieres commandes</h3>
+                        <button className={styles.button}> Go!</button>
+                    </div>
+                    <div className={styles.AllerInventaire}>
+                        <h3> Inventaire</h3>
+                        <button className={styles.button}> Go!</button>
+                    </div>
                 </div>
-            </div>
-
-        </div>
     
-    );
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>ERREUR: vous n'avez pas les permissions pour faire cela.</h1>
+            </div>
+        )
+    }
+
+
 }
