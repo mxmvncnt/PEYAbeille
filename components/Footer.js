05c6afbc8777ec328aaba31330ca1e12b298072f
MihailPeychev <78099428+MihailPeychev@users.noreply.github.com>
2023-03-17 02:45:12 -0400

    modification sur footer, pas fini

--- components/Footer.js
+++ components/Footer.js
@@ -5,29 +5,42 @@ import styles from '../styles/footer.module.css'
 
 export default function Footer() {
     return (
+
         <div className={styles.footer}>
-            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
-                <h3>Liens</h3>
-                <a href="example.com">
-                    <div>Instagram</div>
-                </a>
-                <a href="example.com">
-                    <div>Facebook</div>
-                </a>
+            <div className={`${styles.footer_categorie}`}>
+                <h3>PEYABEILLE</h3>
+                <ul className={`${styles.footer_ul} ${styles.footer_liens}`}>
+                    <li className={`${styles.footer_li}`}> <a href="/"> <div>Acceuil</div> </a> </li>
+                    <li className={`${styles.footer_li}`}> <a href="/apropos"><div>À propos</div> </a></li>
+                    <li className={`${styles.footer_li}`}>  <a href="/produits"><div>Produits</div></a></li>
+                    <li className={`${styles.footer_li}`}> <a href="/produits"><div>Nous joindre</div></a></li>
+                </ul>
             </div>
-            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
+            <div className={`${styles.footer_categorie}`}>
+                <h3>Produits</h3>
+                <ul className={`${styles.footer_ul} ${styles.footer_liens}`}>
+                    <li className={`${styles.footer_li}`}><a href="example.com"><div>Cat 1</div></a></li>
+                    <li className={`${styles.footer_li}`}><a href="example.com"><div>Cat 2</div></a></li>
+                    <li className={`${styles.footer_li}`}><a href="example.com"><div>Cat 3</div></a></li>
+                </ul>
+            </div>
+            <div className={`${styles.footer_categorie}`}>
                 <h3>Politiques</h3>
-                <a href="example.com">
-                    <div>Conditions d'utilisation</div>
-                </a>
+                <ul className={`${styles.footer_ul}`}>
+                    <li className={`${styles.footer_li}`}><a href="example.com"><div>Conditions d'utilisation</div></a></li>
+                    <li className={`${styles.footer_li}`}><a href="example.com"><div>Politique de confidentialité</div></a></li>
+                </ul>
             </div>
-            <div className={`${styles.footer_categorie} ${styles.footer_liens}`}>
+            <div className={`${styles.footer_categorie}`}>
                 <h3>Contact</h3>
-                <form action="">
-                    <label>Courriel</label>
-                    <input type="text"/>
-                </form>
+                <ul className={`${styles.footer_ul} ${styles.footer_liens}`}>
+                    <li className={`${styles.footer_li}`}> <label> Montreal, Canada </label></li>
+                    <li className={`${styles.footer_li}`}> <label> info@peyabeille.com </label></li>
+                    <li className={`${styles.footer_li}`}> <label> 123-456-7891 </label></li>
+                </ul>
             </div>
+            <h7> Copy right</h7>
         </div>
+       
     );
 }
