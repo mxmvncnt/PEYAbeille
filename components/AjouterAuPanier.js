cd0f9da8bdaa1bd932fa9cd74f2fd25762211975
Maxime V <git@maximevincent.com>
2023-03-23 15:45:34 -0400

    Incrementer la qté d'un item quand il est ajouté plus d'une fois

--- components/AjouterAuPanier.js
+++ components/AjouterAuPanier.js
@@ -13,8 +13,27 @@ export default function AjouterAuPanier(item) {
 
         if (cookies["panier"] != undefined) {
             let arrayItems = cookies["panier"]["items_panier"]
-            arrayItems.push(JSON.parse(produitPanierJson))
-            setCookie('panier', {"items_panier" : arrayItems}, { sameSite: true, path: "/" })
+
+            let produitPanier = JSON.parse(produitPanierJson)
+
+            let isItemIncremente;
+            
+            // Incrementer la quantite si l'item est deja dans le panier/quand il est ajouté plus d'une fois
+            for (let i = 0; i < arrayItems.length; i++) {
+                if (arrayItems[i]["item"] == produitPanier["item"]) {
+                    arrayItems[i]["quantite"]++;
+                    setCookie('panier', { "items_panier": arrayItems }, { sameSite: true, path: "/" })
+                    isItemIncremente = true;
+                    break;
+                }
+            }
+
+            if (!isItemIncremente) {
+                arrayItems.push(produitPanier)
+                setCookie('panier', { "items_panier": arrayItems }, { sameSite: true, path: "/" })
+                isItemIncremente = false;  
+            }
+
         }
         else {
             let squeletteJson = { "items_panier": [JSON.parse(produitPanierJson)] }
