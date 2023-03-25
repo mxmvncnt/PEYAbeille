b069c33c57aade900f716001b84f6e17c0596a8e
Maxime V <git@maximevincent.com>
2023-03-25 15:15:46 -0400

    ne pas afficher le contenu du panier si le cookie est vide

--- app/panier/page.js
+++ app/panier/page.js
@@ -17,92 +17,144 @@ const getItemsPanier = () => {
 export default async function Panier() {
     const data = getItemsPanier();
 
-    let dataTotal = data["value"];
-    dataTotal = JSON.parse(dataTotal)
-    let sousTotal = 0;
+    if (data != "Aucune donnee") {
+        let dataTotal = data["value"];
+        dataTotal = JSON.parse(dataTotal)
+        let sousTotal = 0;
 
-    for (let i = 0; i < dataTotal["items_panier"].length; i++) {
-        sousTotal += dataTotal["items_panier"][i]["prix_suggere_unite"] * dataTotal["items_panier"][i]["quantite"];
-    }
+        for (let i = 0; i < dataTotal["items_panier"].length; i++) {
+            sousTotal += dataTotal["items_panier"][i]["prix_suggere_unite"] * dataTotal["items_panier"][i]["quantite"];
+        }
 
-    sousTotal = sousTotal.toFixed(2)
-    let taxesTVQ = (sousTotal * 0.09975).toFixed(2);
-    let taxesTPS = (sousTotal * 0.05).toFixed(2);
-    let taxesTotal = parseFloat(taxesTPS) + parseFloat(taxesTVQ);
-    let prixTotal = parseFloat(sousTotal) + parseFloat(taxesTotal);
+        sousTotal = sousTotal.toFixed(2)
+        let taxesTVQ = (sousTotal * 0.09975).toFixed(2);
+        let taxesTPS = (sousTotal * 0.05).toFixed(2);
+        let taxesTotal = parseFloat(taxesTPS) + parseFloat(taxesTVQ);
+        let prixTotal = parseFloat(sousTotal) + parseFloat(taxesTotal);
 
-    let itemsPanierJson = dataTotal["items_panier"]
+        let itemsPanierJson = dataTotal["items_panier"]
 
-    return (
-        <div className={styles.body}>
-            {/* <h1>Panier</h1> */}
+        return (
+            <div className={styles.body}>
 
-            <div className={styles.sections_panier}>
+                <div className={styles.sections_panier}>
 
-                <div>
-                    <h2 className={styles.titre_section}>Adresse</h2>
-                    <div className={styles.infos_commande}>
-                        <form action={``} method="GET">
+                    <div>
+                        <h2 className={styles.titre_section}>Adresse</h2>
+                        <div className={styles.infos_commande}>
+                            <form action={``} method="GET">
 
-                            <label htmlFor="prenom">Prénom:</label>
-                            <input className={styles.input_field} type="text" id="prenom" name="prenom" />
+                                <label htmlFor="prenom">Prénom:</label>
+                                <input className={styles.input_field} type="text" id="prenom" name="prenom" />
 
-                            <label htmlFor="nom">Nom:</label>
-                            <input className={styles.input_field} type="text" id="nom" name="nom" />
+                                <label htmlFor="nom">Nom:</label>
+                                <input className={styles.input_field} type="text" id="nom" name="nom" />
 
-                            <label htmlFor="numeroRue">Numéro de rue:</label>
-                            <input className={styles.input_field} type="text" id="numeroRue" name="numeroRue" />
+                                <label htmlFor="numeroRue">Numéro de rue:</label>
+                                <input className={styles.input_field} type="text" id="numeroRue" name="numeroRue" />
 
-                            <label htmlFor="rue">Rue:</label>
-                            <input className={styles.input_field} type="text" id="rue" name="rue" />
+                                <label htmlFor="rue">Rue:</label>
+                                <input className={styles.input_field} type="text" id="rue" name="rue" />
 
-                            <input type="submit" value="Confirmer" />
-                        </form>
-                    </div>
-                </div>
-
-
-                <div>
-                    <h2 className={styles.titre_section}>Items</h2>
-                    <div className={styles.carte_items_commande}>
-                        <div className={styles.items_commande}>
-                            <div className={styles.data_items}>
-
-                                {itemsPanierJson.map((item) => (
-                                    <ItemPanier key={item.item} data={item} />
-                                ))}
-
-                            </div>
+                                <input type="submit" value="Confirmer" />
+                            </form>
                         </div>
-                        <div className={styles.infos_total}>
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td className={styles.total_label}><b>Sous-total: </b></td>
-                                        <td className={styles.total_prix}>{sousTotal} $</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={styles.total_label}><b>Taxes: </b></td>
-                                        <td className={styles.total_prix}>{taxesTotal} $</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={`${styles.total_label} ${styles.details_taxes}`}>TPS: </td>
-                                        <td className={styles.total_prix}>{taxesTPS} $</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={`${styles.total_label} ${styles.details_taxes}`}>TVQ: </td>
-                                        <td className={styles.total_prix}>{taxesTVQ} $</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={styles.total_label}><b>Total: </b></td>
-                                        <td className={styles.total_prix}>{prixTotal} $</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    </div>
+
+
+                    <div>
+                        <h2 className={styles.titre_section}>Items</h2>
+                        <div className={styles.carte_items_commande}>
+                            <div className={styles.items_commande}>
+                                <div className={styles.data_items}>
+
+                                    {itemsPanierJson.map((item) => (
+                                        <ItemPanier key={item.item} data={item} />
+                                    ))}
+
+                                </div>
+                            </div>
+                            <div className={styles.infos_total}>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td className={styles.total_label}><b>Sous-total: </b></td>
+                                            <td className={styles.total_prix}>{sousTotal} $</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={styles.total_label}><b>Taxes: </b></td>
+                                            <td className={styles.total_prix}>{taxesTotal} $</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${styles.total_label} ${styles.details_taxes}`}>TPS: </td>
+                                            <td className={styles.total_prix}>{taxesTPS} $</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${styles.total_label} ${styles.details_taxes}`}>TVQ: </td>
+                                            <td className={styles.total_prix}>{taxesTVQ} $</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={styles.total_label}><b>Total: </b></td>
+                                            <td className={styles.total_prix}>{prixTotal} $</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className={styles.body}>
+                <div className={styles.sections_panier}>
+
+                    <div>
+                        <h2 className={styles.titre_section}>Adresse</h2>
+                        <div className={styles.infos_commande}>
+                            <form action={``} method="GET">
+
+                                <label htmlFor="prenom">Prénom:</label>
+                                <input className={styles.input_field} disabled />
+
+                                <label htmlFor="nom">Nom:</label>
+                                <input className={styles.input_field} disabled />
+
+                                <label htmlFor="numeroRue">Numéro de rue:</label>
+                                <input className={styles.input_field} disabled />
+
+                                <label htmlFor="rue">Rue:</label>
+                                <input className={styles.input_field} disabled />
+
+                                <input value="Confirmer" disabled/>
+                            </form>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <h2 className={styles.titre_section}>Items</h2>
+                        <div className={styles.carte_items_commande}>
+                            <div className={styles.items_commande}>
+                                <div className={styles.data_items}>
+
+                                    <h2>
+                                        Aucun produit dans le panier.
+                                    </h2>
+
+                                </div>
+                            </div>
+                            <div className={styles.infos_total}>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+
+
 }
