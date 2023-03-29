71c1f603e693fe1cf12fce849edd8c8e8d6ece71
Maxime Vincent <git@maximevincent.com>
2023-03-28 23:56:30 -0400

    Refaire le CSS de la page Inventaire

--- app/admin/inventaire/page.js
+++ app/admin/inventaire/page.js
@@ -8,7 +8,7 @@ export default function inventaire() {
         <div>
             <h1> Inventaire</h1>
             <button>Ajouter produit</button>
-            <Inventaire/>
+            <Inventaire />
         </div>
 
     );
@@ -16,29 +16,31 @@ export default function inventaire() {
 
 function Inventaire() {
     return (
-        <div className={styles.ContainerInventaire}>
+        <div style={{minHeight:"100vh"}}>
+            <div className={styles.ContainerInventaire}>
 
-            <div className={styles.form}>
-                <form>
-                    <label>Nom: </label>
-                    <input></input> <br />
-                    <label>Quantitée disponible: </label>
-                    <input type="number" min="0"></input><br />
-                    <label>Prix unitaire: </label>
-                    <input type="number"></input> $ <br />
-                    <label>Description du produit: </label>
-                    <textarea></textarea><br />
-                </form>
+                <div className={styles.form}>
+                    <form>
+                        <label>Nom: </label>
+                        <input className={styles.input_field}></input> <br />
+                        <label>Quantitée disponible: </label>
+                        <input className={styles.input_field} type="number" min="0"></input><br />
+                        <label>Prix unitaire: </label>
+                        <input className={styles.input_field} type="number"></input> $ <br />
+                        <label>Description du produit: </label>
+                        <textarea className={styles.input_field}></textarea><br />
+                    </form>
+                </div>
+                <div className={styles.ContainerImage}>
+                    <p> Ajouter une image</p>
+                    <button>Ajouter Image</button>
+                </div>
+                <div>
+                    <button className={styles.bttnSupprimer}> Supprimer produit</button>
+                    <button className={styles.bttnConfirmer}>Confirmer</button>
+                </div>
+
             </div>
-            <div className={styles.ContainerImage}>
-                <p> Ajouter une image</p>
-                <button>Ajouter Image</button>
-            </div>
-            <div>
-            <button className={styles.bttnSupprimer}> Supprimer produit</button>
-            <button className={styles.bttnConfirmer}>Confirmer</button>
-            </div>
-            
         </div>
     )
 }
