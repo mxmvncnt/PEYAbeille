1a6c54ddae6cbb76df7adfbbd3282f7fddb6a0cf
Maxime V <git@maximevincent.com>
2023-04-05 16:02:04 -0400

    Ajouter image placeholder pour la liste de produits

--- app/produits/page.js
+++ app/produits/page.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { getProduits } from "../../server/Api";
 import IndicateurPrix from "../../components/IndicateurPrix";
 import styles from '../../styles/produits.module.css'
+import Image from "next/image";
+import img_not_found from '../../public/no_img_found.png'
 
 export default async function Produits() {
     const data = await getProduits();
@@ -15,8 +17,8 @@ export default async function Produits() {
             <div className={styles.page_produits_grid_produits}>
                 {data.map((item) => (
                     <div key={item["ID_PRODUIT"]}>
-
                         <div className={styles.page_produits_produit}>
+                            <Image className={styles.thumbnail} src={img_not_found} alt="Aucune image n'à été trouvée pour ce produit" />
                             <h2 className={styles.page_produits_titre}>
                                 {item["NOM"]}
                             </h2>
