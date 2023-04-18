81a1a4e65a563377852ab766ca7a7923539624f1
Mihaja514 <67440213+Mihaja514@users.noreply.github.com>
2023-04-18 02:35:34 -0400

    boutons mon compte + background

--- app/compte/page.js
+++ app/compte/page.js
@@ -2,12 +2,26 @@ import React from "react";
 import styles from '../../styles/compte.module.css';
 import Informations from "../../components/InformationsCompte";
 import UneCommande from "../../components/UneCommande";
+import fleurs from '../../public/fleurs.jpg'
+import Image from "next/image";
 
 export default function Compte() {
     return (
         <div>
             <div className={styles.container}>
-                <h1> Mon compte</h1>
+                <Image className= {styles.imageFleurs}
+                 src={fleurs}/>
+                 <div className={styles.overlay}>
+                    <h1> Mon compte</h1>
+                    <div className={styles.bouttons}>
+                        <button className={styles.btn}>Modifier mes informations </button>
+                        &nbsp;
+                        <button className={styles.btn}>Mes commandes </button>
+                        &nbsp;
+                        <button className={styles.btn}>Se déconnecter </button>
+                    </div>
+                 </div>
+                
             </div>
             <Informations />
             <div className={styles.containerCommande}>
