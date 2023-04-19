55da2d05792f340636a0893eb60a1db56f6b73d3
Mihaja514 <mirabetafika@gmail.com>
2023-04-19 15:38:12 -0400

    logout

--- components/BoutonLogOut.js
+++ components/BoutonLogOut.js
@@ -1,5 +1,17 @@
 "use client";
 import React from "react";
+import {useCookies} from 'react-cookies'
 
+function LogOut() {
+    const [cookie, setCookie, removeCookie] = useCookies();
+    const Logout = () =>{
+        removeCookie('token', {path:'/'})
+    }
+    return(
+        <div>
+            <button onClick={Logout}>Logout</button>
+        </div>
+    )
+}
 
-export default function LogOut() 
+export default LogOut();
