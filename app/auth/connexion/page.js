aab985148e8c8bdb80c2f76e43ff5c4e03c65103
Maxime Vincent <git@maximevincent.com>
2023-04-19 16:07:56 -0400

    Ajouter style a la page login

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -1,21 +1,100 @@
-import React from "react";
-import styles from '../../../styles/connexion.module.css';
+"use client";
 
+import React, { useState } from 'react';
+// import '../../../styles/connexioninscription.module.css';
+import styles from '../../../styles/connexioninscription.module.css';
 
-export default function Connexion() {
+function Connexion() {
+    const [isRegister, setIsRegister] = useState(false);
+
+    const toggleForm = () => {
+        setIsRegister(!isRegister);
+    };
+
     return (
         <div className={styles.body}>
 
-            <div className={styles.container}>
-                <h1>Connexion</h1>
-                <form action={`/api/login`} method="GET">
-                    <input className={styles.input} type="email" id="courriel" name="email" placeholder="Courriel" />
-                    <br></br>
-                    <label htmlFor="mdp">Mot de passe:</label>
-                    <input type="password" id="mdp" name="password" />
-                    <input type="submit" value="Envoyer" />
-                </form>
+
+            <div className={styles.wrap}>
+                <div className={`${styles.form_box} ${styles.login} ${isRegister ? 'hide' : ''}`}>
+                    <h2>Connexion</h2>
+                    <form>
+                        <div className={styles.input_box}>
+                            <span className={styles.icon}></span>
+                            <input type="email" required />
+                            <label>Email</label>
+                        </div>
+                        <div className={styles.input_box}>
+                            <span className={styles.icon}></span>
+                            <input type="password" required />
+                            <label>Mot de passe</label>
+                        </div>
+                        <button type="submit" className={styles.btn}>
+                            Se Connecter
+                        </button>
+                        <div className={styles.login_register}>
+                            <p>
+                                Devenir membre?
+                                <br />
+                                <button type="button" className={styles.register_link} onClick={toggleForm}>
+                                    Créer un compte
+                                </button>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+                <div className={`${styles.form_box} ${styles.login} ${isRegister ? 'show' : ''}`}>
+                    <h2>Inscription</h2>
+                    <form action={`/api/register`} method="GET">
+
+                        <div className={styles.input_box}>
+                            <label htmlFor="prenom">Prénom:</label>
+                            <input type="text" id="prenom" name="prenom" />
+                        </div>
+
+                        <div className={styles.input_box}>
+                            <label htmlFor="nom">Nom:</label>
+                            <input type="text" id="nom" name="nom" />
+                        </div>
+
+                        <div className={styles.input_box}>
+                            <label htmlFor="courriel">Courriel:</label>
+                            <input type="email" id="courriel" name="email" />
+                        </div>
+
+                        <div className={styles.input_box}>
+                            <label htmlFor="mdp">Mot de passe:</label>
+                            <input type="password" id="mdp" name="password" />
+                        </div>
+
+                        <div className={styles.input_box}>
+                            <label htmlFor="mdp">Confirmez le mot de passe:</label>
+                            <input type="password" id="mdp" name="password" />
+                        </div>
+
+                        <div className={styles.remember_forgot}>
+                            <label>
+                                <input type="checkbox" />
+                                Terme & Confidentialite
+                            </label>
+                        </div>
+
+                        <input type="submit" value="S'enregistrer" className={styles.btn} />
+
+                        <div className={styles.login_register}>
+                            <p>
+                                Déjà membre ?
+                                <button type="button" className={styles.login_link} onClick={toggleForm}>
+                                    Se connecter
+                                </button>
+                            </p>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
     );
 }
+
+export default Connexion;
