c246a4c1772473b8eca87523d046d5df13596219
Maxime Vincent <git@maximevincent.com>
2023-04-26 15:42:02 -0400

    Ajouter front-end tri des produits

--- app/produits/page.js
+++ app/produits/page.js
@@ -6,14 +6,27 @@ import IndicateurPrix from "../../components/IndicateurPrix";
 import styles from '../../styles/produits.module.css'
 import ImageProduit from "../../components/ImageProduit";
 
-export default async function Produits() {
-    const data = await getProduits(1);
 
-    console.log(data)
+export default async function Produits({searchParams}) {
+    const sort = parseInt(searchParams["sort"])
+
+    const data = await getProduits(sort);
 
     return (
         <div className={styles.body}>
             <h1>Produits</h1>
+
+            <form>
+                <label>Trier les produits:</label>
+                <select name="sort" id="sort">
+                    <option value="0" selected={sort == NaN ? false : true}>Par défaut</option>
+                    <option value="1" selected={sort == 1 ? true : false}>Prix croissant ($ -{'>'} $$$)</option>
+                    <option value="2" selected={sort == 2 ? true : false}>Prix décroissant ($$$ -{'>'} $)</option>
+                    <option value="3" selected={sort == 3 ? true : false}>Ordre alphabétique (A -{'>'} Z)</option>
+                    <option value="4" selected={sort == 4 ? true : false}>Ordre alphabétique inverse (Z -{'>'} A)</option>
+                </select>
+                <input type="submit" value="Submit" />
+            </form>
 
             <div className={styles.grid_layout}>
                 {data.map((item) => (
