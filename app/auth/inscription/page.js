3aa5483ab8bfab3d987623b8c24e234465328adf
Maxime Vincent <git@maximevincent.com>
2023-04-27 16:58:32 -0400

    ajouter style page connexion

--- app/auth/inscription/page.js
+++ app/auth/inscription/page.js
@@ -1,26 +1,41 @@
 import React from "react";
 import styles from '../../../styles/inscription.module.css';
+import '../../global.css'
 
 export default function Inscription() {
     return (
-        <div className={styles.nom_classe}>
-            <h1>Inscription</h1>
-            <form action={`/api/register`} method="GET">
+        <div className={styles.body}>
+            <div className={styles.card}>
+                <h1>Inscription</h1>
+                <form action={`/api/register`} method="GET">
 
-                <label htmlFor="prenom">Prénom:</label>
-                <input type="text" id="prenom" name="prenom" />
+                    <div className={styles.card_item}>
+                        <label htmlFor="prenom">Prénom:</label>
+                        <input className="input-field-singlerow" type="text" id="prenom" name="prenom" />
+                    </div>
 
-                <label htmlFor="nom">Nom:</label>
-                <input type="text" id="nom" name="nom" />
+                    <div className={styles.card_item}>
+                        <label htmlFor="nom">Nom:</label>
+                        <input className="input-field-singlerow" type="text" id="nom" name="nom" />
+                    </div>
 
-                <label htmlFor="courriel">Courriel:</label>
-                <input type="email" id="courriel" name="email" />
+                    <div className={styles.card_item}>
+                        <label htmlFor="courriel">Courriel:</label>
+                        <input className="input-field-singlerow" type="email" id="courriel" name="email" />
+                    </div>
 
-                <label htmlFor="mdp">Mot de passe:</label>
-                <input type="password" id="mdp" name="password" />
+                    <div className={styles.card_item}>
+                        <label htmlFor="mdp">Mot de passe:</label>
+                        <input className="input-field-singlerow" type="password" id="mdp" name="password" />
+                    </div>
 
-                <input type="submit" value="Envoyer" />
-            </form>
+                    <div className={styles.card_item}>
+                        <button type="submit" className={styles.btn_fullwidth}>
+                            S'inscrire
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
