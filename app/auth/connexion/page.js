11049409269fc046eed2642ca84e70ca72d13863
Maxime Vincent <git@maximevincent.com>
2023-04-27 16:58:11 -0400

    ajouter footer et header a la card de login

--- app/auth/connexion/page.js
+++ app/auth/connexion/page.js
@@ -1,3 +1,4 @@
+import React from "react";
 // import styles from '../../../styles/connexioninscription.module.css';
 import styles from '../../../styles/connexion.module.css';
 import '../../global.css'
@@ -7,19 +8,30 @@ function Connexion() {
     return (
         <div className={styles.body}>
             <div className={styles.card}>
+
+                <div className={styles.card_header}>
+                    <h2 className={styles.card_title}>Connexion</h2>
+                </div>
+
                 <form action={`/api/login`} method="GET">
-                    <div>
+                    <div className={styles.card_item}>
                         <label htmlFor="courriel">Courriel:</label>
                         <input className="input-field-singlerow" type="email" id="courriel" name="email" />
                     </div>
 
-                    <div>
+                    <div className={styles.card_item}>
                         <label htmlFor="mdp">Mot de passe:</label>
                         <input className="input-field-singlerow" type="password" id="mdp" name="password" />
                     </div>
-                    <button type="submit" className={styles.btn_fullwidth}>
-                        Se connecter
-                    </button>
+
+                    <div className={styles.card_item}>
+                        <button type="submit" className={styles.btn_fullwidth}>
+                            Se connecter
+                        </button>
+                    </div>
+                    <div className={styles.card_footer}>
+
+                    </div>
                 </form>
             </div>
 
