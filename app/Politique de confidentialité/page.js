0667473ff8b916360018c9299df6a94f95c582af
MihailPeychev <78099428+MihailPeychev@users.noreply.github.com>
2023-05-03 12:24:29 -0400

    modification de information sur page de confidentialite

--- app/Politique de confidentialité/page.js
+++ app/Politique de confidentialité/page.js
@@ -24,7 +24,80 @@ export default function Compte() {
 
                     </p>
                 </div>
-               
+                <div>
+                    <h3>
+                        Formulaires et interactivité:
+                    </h3>
+                    <p>
+                        Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :
+
+                        Formulaire d'inscription au site Web
+                        Formulaire de commande
+                        Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :
+
+                        Suivi de la commande
+                        Informations / Offres promotionnelles
+                        Statistiques
+                        Contact
+                        Vos renseignements sont également collectés par le biais de l’interactivité pouvant s’établir entre vous et notre site Web et ce, de la façon suivante:
+
+                        Commentaires
+                        Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :
+
+                        Statistiques
+                    </p>
+                </div>
+                <div>
+                    <h3>
+                        Droit d’opposition et de retrait
+                    </h3>
+                    <p>
+                        Nous nous engageons à vous offrir un droit d’opposition et de retrait quant à vos renseignements personnels.
+                        Le droit d’opposition s’entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.
+
+                        Le droit de retrait s’entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.
+                    </p>
+                </div>
+                <div>
+                    <h3>
+                        Droit d’accès
+                    </h3>
+                    <p>
+                        Nous nous engageons à reconnaître un droit d’accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.
+
+                        L’exercice de ce droit se fera :
+
+                        Code postal :  H452G7
+                        Courriel :  info@peyabeille.com
+                        Téléphone :  123-456-7891
+                        Section du site web :   Politque de confidentialité
+                    </p>
+                </div>
+                <div>
+                    <h3>
+                        Sécurité
+                    </h3>
+                    <p>
+                        Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé. Les personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.
+
+                        Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes :
+
+                        Gestion des accès - personne autorisée
+                        Sauvegarde informatique
+                        Identifiant / mot de passe
+                        Pare-feu
+                        Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d’assurer la confidentialité de vos transactions. Toutefois, comme aucun mécanisme n’offre une sécurité maximale, une part de risque est toujours présente lorsque l’on utilise Internet pour transmettre des renseignements personnels.
+                    </p>
+                </div>
+                <div>
+                    <h3>Législation
+                    </h3>
+                    <p>
+                        Nous nous engageons à respecter les dispositions législatives énoncées dans :
+
+                        Législation: Peyabeille
+                    </p>
+                </div>
 
             </div>
 
