b84624cf830e7e8595eb24e18c3037b1a6461a97
Maxime Vincent <git@maximevincent.com>
2023-05-11 14:22:19 -0400

    utiliser h3 pour les sous titres de la carte message

--- components/MessageAdmin.js
+++ components/MessageAdmin.js
@@ -3,30 +3,28 @@ import styles from '../styles/MessageAdmin.module.css'
 
 export default function MessageAdmin(data) {
 
-    // console.log(data)
     const jsonData = data["data"]
-    console.log(jsonData)
 
     return (
         <div className={styles.message}>
             <div className={styles.nom}>
-                <h2>Nom :</h2>
+                <h3>Nom :</h3>
                 <p>{jsonData.nom}</p>
             </div>
             <div className={styles.prenom}>
-                <h2>Prénom: </h2>
+                <h3>Prénom: </h3>
                 <p>{jsonData.prenom}</p>
             </div>
             <div className={styles.email}>
-                <h2>Courriel:</h2>
+                <h3>Courriel:</h3>
                 <p>{jsonData.email}</p>
             </div>
             <div className={styles.sujet}>
-                <h2>Sujet:</h2>
+                <h3>Sujet:</h3>
                 <p>{jsonData.sujet}</p>
             </div>
             <div className={styles.contenu}>
-                <h2>Message:</h2>
+                <h3>Message:</h3>
                 <p>{jsonData.message}</p>
             </div>
             <div className={styles.boutonMailTo}>
