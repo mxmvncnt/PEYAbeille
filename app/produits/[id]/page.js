a4d5accbaa9d27c795d8f3233e7b3cd747f091e9
Maxime Vincent <git@maximevincent.com>
2023-05-11 16:33:59 -0400

    Modifier layout d'un produit
    - Afficher prix sous description
    - plus grande image

--- app/produits/[id]/page.js
+++ app/produits/[id]/page.js
@@ -18,26 +18,25 @@ export default async function Produit({ params }) {
 
                 <CarouselImageProduit id={params.id} />
 
-                {/* Section Infos Produit */}
-                <div className={`${styles.page_produit_grid_item} ${styles.page_produit_grid_item_infos}`}>
-                    <h1>{data["NOM"]}</h1>
-                    <p>{data["DESCRIPTION"]}</p>
-                </div>
-
-                {/* Section ACHETER */}
-                <div className={`${styles.page_produit_grid_item} ${styles.page_produit_grid_item_acheter}`}>
-                    <div>
-                        <IndicateurPrix prix_regulier={data["PRIX_FIXE"]} prix_suggere={data["PRIX_SUGGERE"]} />
-                        
-                        <AjouterAuPanier item={await data["ID_PRODUIT"]} nom_produit={await data["NOM"]} prix_suggere_unite={await data["PRIX_SUGGERE"]} quantite={1} />
-                        
-                        <small>En stock: {data["QUANTITE"]}</small>
+                <div className={styles.page_produit_grid_item}>
+                    {/* Section Infos Produit */}
+                    <div className={`${styles.page_produit_grid_item_infos}`}>
+                        <h1>{data["NOM"]}</h1>
+                        <p>{data["DESCRIPTION"]}</p>
                     </div>
 
+                    {/* Section ACHETER */}
+                    <div className={`${styles.page_produit_grid_item_acheter}`}>
+                        <div>
+                            <IndicateurPrix prix_regulier={data["PRIX_FIXE"]} prix_suggere={data["PRIX_SUGGERE"]} />
+
+                            <AjouterAuPanier item={await data["ID_PRODUIT"]} nom_produit={await data["NOM"]} prix_suggere_unite={await data["PRIX_SUGGERE"]} quantite={1} />
+
+                            <small>En stock: {data["QUANTITE"]}</small>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
         </div>
     );
 }
